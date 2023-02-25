19e6c586747f72d823816ee6f91f87b4472eaa5d
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-02-25 12:13:51 +0900

    test: "satisfies" not supported

--- src/transform-isort.test.ts
+++ src/transform-isort.test.ts
@@ -2,8 +2,18 @@ import { applyTransform } from "jscodeshift/src/testUtils";
 import { describe, expect, it } from "vitest";
 import { transformIsort } from "./transform-isort";
 
+function transformWrapper(input: string): string {
+  const output = applyTransform(
+    transformIsort,
+    {},
+    { source: input },
+    { parser: "tsx" }
+  );
+  return output;
+}
+
 describe("transformIsort", () => {
-  it("basic", () => {
+  it("prettier-ignore", () => {
     const input = `\
 import { y0, x0 } from "e";
 import { y1, x1 } from "d";
@@ -12,12 +22,7 @@ import { y2, x2 } from "a";
 import { y3, x3 } from "c";
 import { y4, type x4 } from "b";
 `;
-    const output = applyTransform(
-      transformIsort,
-      {},
-      { source: input },
-      { parser: "tsx" }
-    );
+    const output = transformWrapper(input);
     expect(output).toMatchInlineSnapshot(`
       "import { x1, y1 } from \\"d\\";
       import { x0, y0 } from \\"e\\";
@@ -26,5 +31,22 @@ import { y4, type x4 } from "b";
       import { type x4, y4 } from \\"b\\";
       import { x3, y3 } from \\"c\\";"
     `);
+  });
+
+  describe("syntax", () => {
+    it("satisfies", () => {
+      const input = `\
+import { y, x } from "b";
+import "a";
+
+const x = { k: 0 } satisfies Record<string, number>;
+`;
+      expect(() => {
+        const output = transformWrapper(input);
+        expect(output).toMatchInlineSnapshot();
+      }).toThrowErrorMatchingInlineSnapshot(
+        '"did not recognize object of type \\"TSSatisfiesExpression\\""'
+      );
+    });
   });
 });
